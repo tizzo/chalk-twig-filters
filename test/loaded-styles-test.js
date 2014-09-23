e344fbc9b376519049c5515fda1b3d04f5c79e28
Howard Tyson <howard@howardtyson.com>
2014-09-23 18:49:45 -0400

    Adding a test to get coverage of branches to 100%.

--- test/loaded-styles-test.js
+++ test/loaded-styles-test.js
@@ -12,4 +12,12 @@ describe('attach styles', function() {
     var output = template.render({text: 'foo'});
     output.should.equal(chalk.red.underline.bold('foo'));
   });
+  it('should not error if no content is present in message', function() {
+    attachFilters(Twig);
+    var template = Twig.twig({
+      data: '{{ text | red | underline | bold }}'
+    });
+    var output = template.render({text: null});
+    output.should.equal(chalk.red.underline.bold(''));
+  });
 });
